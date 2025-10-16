017ff2b363a0bc905a11f45b62862eef6b417ebb
VictorLCosta <victorlc2019@outlook.com>
2025-10-16 15:29:10 -0300

    refactor: use config for server port and add configuration setup

--- src/server.ts
+++ src/server.ts
@@ -1,3 +1,4 @@
 import app from "./app";
+import config from "./config";
 
-app.listen(3000, () => console.log("Server running on port 3000"));
+app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
