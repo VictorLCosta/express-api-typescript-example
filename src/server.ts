e02d79336958bfdef25290973163a424fd29ad33
VictorLCosta <victorlc2019@outlook.com>
2025-10-19 23:46:53 -0300

    feat: implement server shutdown on unhandled promise rejection

--- src/server.ts
+++ src/server.ts
@@ -1,4 +1,12 @@
 import app from "./app";
 import config from "./config";
 
-app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
+const server = app.listen(config.port, () => console.log(`Server running on port ${config.port}`));
+
+process.on('unhandledRejection', err => {
+    console.log(`Error: ${(err as any).message}`);
+    console.log('Shutting down the server due to Unhandled promise rejection.')
+    server.close( () => {
+        process.exit(1);
+    }) 
+});
