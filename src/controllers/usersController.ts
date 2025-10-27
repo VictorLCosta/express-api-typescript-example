915b0dd8b2de942cc95799460755c5df20ed6737
VictorLCosta <victorlc2019@outlook.com>
2025-10-26 21:16:26 -0300

    feat: add update password functionality to user routes and controller

--- src/controllers/usersController.ts
+++ src/controllers/usersController.ts
@@ -1,4 +1,7 @@
+import { db } from "@/lib/db";
 import { asyncMiddleware } from "@/middlewares/asyncHandler";
+import { comparePassword, User } from "@/models";
+import { sendToken } from "@/utils/jwtToken";
 
 export const getCurrentUser = asyncMiddleware(async (req, res, next) => {
   const user = req.user;
@@ -7,4 +10,26 @@ export const getCurrentUser = asyncMiddleware(async (req, res, next) => {
     return res.status(404).json({ message: "User not found" });
 
   res.status(200).json({ user });
+});
+
+export const updatePassword = asyncMiddleware(async (req, res, next) => {
+  if (!req.user)
+    return res.status(404).json({ message: "User not found" });
+
+  const result = await db<User>("users")
+    .where({ email: req.user.email })
+    .select("password")
+    .first();
+
+  const isMatched = comparePassword(result!.password, req.user.password);
+
+  if (!isMatched) {
+    return res.status(401).json({ message: "Old password is incorrect" });
+  }
+
+  await db<User>("users")
+    .where({ email: req.user.email })
+    .update({ password: req.body.newPassword });
+
+  return sendToken(req.user as User, 200, res, req);
 });
--- src/routes/userRoutes.ts
+++ src/routes/userRoutes.ts
@@ -1,9 +1,10 @@
 import { Router } from "express";
 
-import { getCurrentUser } from "@/controllers/usersController";
+import { getCurrentUser, updatePassword } from "@/controllers/usersController";
 
 const router = Router();
 
 router.get("/me", getCurrentUser);
+router.put("/me/password", updatePassword);
 
 export default router;
